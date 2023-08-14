7a4da4967b8ae5c82c52b460296fc82755cbba51
4862348 <4862348@UBILYZLSMD017>
2023-08-14 10:37:57 +0300

    Add project files.

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace MyDictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+            myDictionary.Add(1, "a");
+            myDictionary.Add(2, "b");
+            myDictionary.Add(3, "c");
+            myDictionary.PrintKeysAndValues();
+            Console.WriteLine("Count: " + myDictionary.Count);
+        }
+    }
+}
